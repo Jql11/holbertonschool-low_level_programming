8789d7fd05a4a73121b86ad63df2d09f6c8d6b12
Jql11 <4325@holbertonschool.com>
2022-02-03 15:38:41 +1100

    fix betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main - first function in the program
+ * puts - The puts function proceeds to write the contents of the strings
+ * Return: if success then return 0
+ */
+int main(void)
 {
 char str1[] = "Programming is like building a multilingual puzzle";
 puts(str1);
- return(0);
+return (0);
 }
-
