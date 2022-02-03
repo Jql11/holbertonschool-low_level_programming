d27c2bde09e1824593e172d8799146249dd258fa
Jql11 <4325@holbertonschool.com>
2022-02-03 15:59:50 +1100

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - first function in the program
+ * printf - to display the output
+ * Return: if success then return 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
