9fdad42f31778f91432cdf854a71cab443a6c097
Jql11 <4325@holbertonschool.com>
2022-02-04 17:17:14 +1100

    if

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - first function in the program
+ * Return: if success then return 0
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	for (i = 48; i <= 56; i++)
+	{
+		putchar(i);
+	putchar(',');
+	putchar(' ');
+	}
+	if (i == 57)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+
+	return (0);
+}
