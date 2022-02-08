bdbf976342bce195a21eb71e9aab7c2cb071a0a8
Jql11 <4325@holbertonschool.com>
2022-02-08 21:27:58 +1100

    answer

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <ctype.h>
+#include<stdio.h>
+/**
+ *add - add integers
+ *@a: integer
+ *@b: integer
+ *Return: Always 0 (Success)
+ */
+
+int add(int a, int b)
+{
+		return (a + b);
+}
