2225bb4f744a967ee1f2eed27052eaddab0a05dd
Jql11 <4325@holbertonschool.com>
2022-02-08 12:39:58 +1100

    first commit

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and error is set appropriately.
+ */
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+#endif
