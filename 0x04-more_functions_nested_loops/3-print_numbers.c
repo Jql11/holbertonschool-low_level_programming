4b94b8d6e8582846b876f926716b97bb8e7a41e6
Jql11 <4325@holbertonschool.com>
2022-02-10 10:45:46 +1100

    task 3 fix logic

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+*print_numbers - prints the numbers from 0 to 9
+*
+*
+*Return: Always 0 (Success)
+*/
+
+void print_numbers(void)
+{
+	int a = 48;
+
+	while (a <= 57)
+	{
+		_putchar(a);
+		a++;
+	}
+	_putchar('\n');
+}
