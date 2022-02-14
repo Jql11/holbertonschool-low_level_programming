8e39a58fd53de71a912a5057647d80bd89d4f7b6
Jql11 <4325@holbertonschool.com>
2022-02-14 17:01:12 +1100

    task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ *@str: string
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+	int length;
+	int i;
+
+	length = _strlen(str);
+	for (i = 0; i < length; i++)
+	{
+		if (length != 0)
+		{
+			for (i = (length + 1) / 2; i < length & str[i] != '\0'; i++)
+			{
+				_putchar(str[i]);
+			}
+		}
+		else
+		{
+			for (i = (length / 2) + 1; i < length & str[i] != '\0'; i++)
+					_putchar(str[i]);
+		}
+		_putchar('\n');
+	}
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+
