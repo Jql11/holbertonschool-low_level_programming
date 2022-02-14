ae2e2104410098d867045c72cf8f41cec3092066
Jql11 <4325@holbertonschool.com>
2022-02-14 11:32:38 +1100

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - takes a pointer to an int and it points to 98
+ *@n: integer
+ *Return: Always 0 (Success)
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+
+}
