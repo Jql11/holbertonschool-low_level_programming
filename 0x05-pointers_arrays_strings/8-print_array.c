e3f36034f467444a1fb27158adbe4003accc510e
Jql11 <4325@holbertonschool.com>
2022-02-14 17:41:56 +1100

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_array - print array
+ *@a: integer
+ *@n: integer
+ * Return: Always 0.
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i == n - 1)
+		{
+			printf("%d\n", a[i]);
+		}
+		else
+		{
+			printf("%d, ", a[i]);
+		}
+	}
+}
