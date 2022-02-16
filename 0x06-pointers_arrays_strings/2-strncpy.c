e2beccac79aceecfc392702ddeac3d8492ea09fb
Jql11 <4325@holbertonschool.com>
2022-02-17 09:25:25 +1100

    betty

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,6 +11,7 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
+
 	while (*(src + i) != '\0' && i < n)
 	{
 		dest[i] = src[i];
@@ -18,8 +19,8 @@ char *_strncpy(char *dest, char *src, int n)
 	}
 	while (i < n)
 	{
-		 dest[i] = ('\0');
-		 i++;
+		dest[i] = ('\0');
+		i++;
 	}
 	return (dest);
 }
