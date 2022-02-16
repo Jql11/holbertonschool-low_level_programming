aba341cea6183ad8ef1c56af018c4bd76f8959ae
Jql11 <4325@holbertonschool.com>
2022-02-16 18:18:27 +1100

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ *@s: string
+ *
+ *
+ * Return: Always 0.
+ */
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] = s[i] - 'a' + 'A';
+		}
+		i++;
+	}
+	return (s);
+}
