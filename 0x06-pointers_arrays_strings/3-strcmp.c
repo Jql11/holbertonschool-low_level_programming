5135d479bbfbee6aa66311c8c1646e36b7641484
Jql11 <4325@holbertonschool.com>
2022-02-16 18:05:04 +1100

    betty

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,19 +1,22 @@
 #include "main.h"
 /**
- * _strcmp - compares two strings. 
- * 
- * @s1: 
- * @s2:
+ * _strcmp - compares two strings.
+ *
+ * @s1:string 1
+ * @s2:string 2
  * Return: copied string
  */
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
+
 	while (*(s1 + i) != '\0' && *(s2 + i) != '\0')
 	{
 		if (s1[i] != s2[i])
+		{
 			return (s1[i] - s2[i]);
-		else 
+		}
+		else
 			i++;
 	}
 	return (0);
