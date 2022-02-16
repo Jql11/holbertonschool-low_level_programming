9786b6fb577e6f5655bad23ee561f6e7a1579b16
Jql11 <4325@holbertonschool.com>
2022-02-17 09:22:54 +1100

    betty

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -12,16 +12,15 @@ char *_strncat(char *dest, char *src, int n)
 	int i = 0;
 	int j = 0;
 
-	while(*(dest + j) != '\0')
+	while (*(dest + j) != '\0')
 	{
 		j++;
 	}
-
 	while (*(src + i) != '\0' && i < n)
 	{
-			dest[j] = src[i];
-			i++;
-			j++;
+		dest[j] = src[i];
+		i++;
+		j++;
 	}
 	dest[j] = ('\0');
 	return (dest);
