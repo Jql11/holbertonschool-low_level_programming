972aba818d166aa97b6a0d973011d07cb97490bb
Jql11 <4325@holbertonschool.com>
2022-02-17 12:56:22 +1100

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalise all words of a string
+ *@s: string
+ * Return: Always 0.
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] >= 'A' && s[i] <= 'Z')
+			s[i] = s[i];
+		else if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+		{
+			if (s[i] == '"' || s[i] == '(' || s[i] == ')' ||
+					s[i] == '{' || s[i] == '}' ||
+					s[i] == '\n' || s[i] == '\t' ||
+					s[i] == '.' || s[i] == '?' ||
+					s[i] == ';' || s[i] == '!' ||
+					s[i] == ',' || s[i] == ' ' || 
+					s[i] == '.')
+			{
+				s[i + 1] = s[i + 1] - 'a' + 'A';
+			}
+		}
+		i++;
+	}
+	return (s);
+
+}
+
