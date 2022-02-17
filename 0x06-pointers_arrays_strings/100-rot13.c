2bced6f3b37589bf305d587d4fa1e64cfc878a14
Jql11 <4325@holbertonschool.com>
2022-02-17 17:51:28 +1100

    fix while loop

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * rot13 - encode a string
+ *@s: string
+ * Return: Always 0.
+ */
+
+
+char *rot13(char *s)
+{
+	int a[53] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
+		     'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x',
+		     'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+		     'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V',
+		     'W', 'X', 'Y', 'Z'};
+	int b[53] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+		     'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+		     'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W',
+		     'X', 'Y', 'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
+		     'J', 'K', 'L', 'M'};
+	int i; 
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		j = 0;
+		while (a[j] != '\0' && s[i] != a[j])
+			j++;
+
+		if (s[i] == a[j])
+			s[i] = b[j];
+	}
+	return (s);
+}
