4f30ff4725f2225077c37911160d16690b253041
Jql11 <4325@holbertonschool.com>
2022-02-21 12:29:42 +1100

    fix code

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,12 +9,13 @@
 
 char *_strchr(char *s, char c)
 {
-	while (*s)
+	while (*s != '\0')
 	{
-		if (*s != '\0' && *s != c)
-			s++;
-		else
+		if (*s == c)
 			return (s);
+		s++;
 	}
+	if (*s == '\0')
+		return (s);
 	return (NULL);
 }
