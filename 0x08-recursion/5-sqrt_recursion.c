a6af80061b1feda33ec6acd1cf5d3ba8bf27257b
Jql11 <4325@holbertonschool.com>
2022-02-23 19:34:30 +1100

    task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+*square - find the natural square root of a number
+*@k - mutiplier
+*@n - number
+*Return: return value
+*/
+
+int square(int k, int n)
+{
+	if (k * k == n)
+		return (k);
+	else if (k * k > n)
+		return (-1);
+	else
+		return (square(k + 1, n));
+}
+
+/**
+ * _sqrt_recursion - the natural square root of a number
+ *@n: integer
+ * Return: returns the value
+ */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (0);
+	else 
+		return (square(1, n));
+}
