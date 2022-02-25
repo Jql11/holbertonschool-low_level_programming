7ab7e514b2fbbb6e1b172f1090c9ddbca7b197b2
Jql11 <4325@holbertonschool.com>
2022-02-25 16:33:53 +1100

    betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,11 @@
 # include <stdio.h>
 # include "main.h"
-
+/**
+  *main - main function
+  *@argc: number of arguments
+  *@argv: array of arguments
+  *Return: 0
+  */
 int main(int argc, char *argv[])
 {
 	if (argc > 0)
