50ca19ef7548841146381d8b6dc866b4f8aa3517
Jql11 <4325@holbertonschool.com>
2022-02-25 16:53:22 +1100

    task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+# include "main.h"
+/**
+  *main - main function
+  *@argc: number of arguments
+  *@argv: array of arguments
+  *Return: 0
+  */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc > 0)
+		for (i = 0; i < argc; i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}
