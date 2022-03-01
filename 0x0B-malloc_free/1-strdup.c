31dda7e7088486913da2c1c710159c63763ce361
jql11 <4325@holbertonschool.com>
2022-02-28 19:42:00 -0800

    change pointer method to array method

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *_strlen - returns the length of a string
+ *@s: character
+ *
+ *Return: Always 0 (Success)
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+
+/**
+ * _strdup - a copy of the string
+ *@str: string
+ * Return: returns a pointer tothe duplicated string
+ */
+char *_strdup(char *str)
+{
+	char *p;
+	int length = _strlen(str);
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+	p = malloc(sizeof(char) * (length + 1));
+
+
+	if (p == NULL)
+		return (p);
+	for (i = 0; i < length; i++)
+	{
+		p[i] = str[i];
+	}
+	return (p);
+}
+
