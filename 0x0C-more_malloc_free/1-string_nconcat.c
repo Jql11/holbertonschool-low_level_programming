3ca435fd250ecff6640595f0b4d153a715c9130b
jql11 <4325@holbertonschool.com>
2022-03-02 19:17:03 -0800

    task 1

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,67 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *_strlen - returns the length of a string
+ *@s: character
+ *
+ *Return: Always 0 (Success)
+ */
+unsigned int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+
+	return (len);
+}
+
+/**
+ * string_nconcat - concatenate two string
+ * @s1: string 1
+ * @s2: string 2
+ * @n: n bytes of s2
+ * Return: pointer s1 + n bytes of s2 + null.
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *p;
+	unsigned int length;
+	unsigned int i, j;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (n <= _strlen(s2))
+	{
+		length = _strlen(s1) + n + 1;
+	}
+	else
+		length = _strlen(s1) + _strlen(s2) + 1;
+	p = malloc(length * sizeof(char));
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < _strlen(s1); i++)
+	{
+		p[i] = s1[i];
+	}
+	if (n <= _strlen(s2))
+	{
+		for (j = 0; j < n; j++, i++)
+		{
+			p[i] = s2[j];
+		}
+	}
+	else
+	{
+		for (j = 0; j < _strlen(s2); j++, i++)
+			p[i] = s2[j];
+	}
+	return (p);
+}
