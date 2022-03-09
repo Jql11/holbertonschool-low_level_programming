c4ceb82eea386cb93a3cf58132c9d1322ab78138
jql11 <4325@holbertonschool.com>
2022-03-09 14:17:52 -0800

    task 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,12 +17,12 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	f = get_op_func(argv[2]);
-	if (argv[2] == '\0')
+	if (f == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	else if ((argv[2] == '/' || argv[2] == '%') && argv[3] == 0)
+	else if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == 0)
 	{
 		printf("Error\n");
 		exit(100);
