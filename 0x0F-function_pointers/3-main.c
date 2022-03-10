b9fc0e8f501077d6aef5f4cf5340da6c594707b0
jql11 <4325@holbertonschool.com>
2022-03-10 03:59:41 -0800

    add brackets

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 		exit(98);
 	}
 	f = get_op_func(argv[2]);
-	if (f == NULL || argv[2][1] != '\0')
+	if (f == NULL || (argv[2][1] != '\0'))
 	{
 		printf("Error\n");
 		exit(99);
