c24b22348b07639ff3f5368a7d17ebc211614e5f
jql11 <4325@holbertonschool.com>
2022-03-10 04:26:18 -0800

    change sep location

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -20,12 +20,15 @@ void print_all(const char * const format, ...)
 		{
 			case 'c':
 				printf("%s%c", sep, va_arg(args, int));
+				sep = ", ";
 				break;
 			case 'i':
 				printf("%s%d", sep, va_arg(args, int));
+				sep = ", ";
 				break;
 			case 'f':
 				printf("%s%f", sep, va_arg(args, double));
+				sep = ", ";
 				break;
 			case 's':
 				str = va_arg(args, char *);
@@ -34,9 +37,9 @@ void print_all(const char * const format, ...)
 					str = "(nil)";
 				}
 				printf("%s%s", sep, str);
+				sep = ", ";
 				break;
 		}
-		sep = ", ";
 		i++;
 	}
 	va_end(args);
