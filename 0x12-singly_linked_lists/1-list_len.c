092bc2828e7314390644b6b87a5e12f9da82068b
jql11 <4325@holbertonschool.com>
2022-03-15 23:55:54 -0700

    first commit

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+  *list_len - prints length of list in linked list
+  *@h: linked list
+  *Return: number of nodes in linked list
+  */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
+	return (count);
+}
