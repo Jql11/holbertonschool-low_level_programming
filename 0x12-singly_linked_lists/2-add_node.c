bd63760671f3fbb52cc98978101640cdcb511fbb
jql11 <4325@holbertonschool.com>
2022-03-16 00:48:28 -0700

    betty

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -19,13 +19,15 @@ int _strlen(const char *str)
 
 /**
   *add_node - adds a new node at the beginning of a list_t list
-  *@h: linked list
+  *@head: head
+  *@str: string
   *Return: the address of the new element or NULL if it failed
   */
 list_t *add_node(list_t **head, const char *str)
 {
 
 	list_t *new = malloc(sizeof(list_t));
+
 	if (new == NULL)
 		return (NULL);
 	if (str == NULL)
