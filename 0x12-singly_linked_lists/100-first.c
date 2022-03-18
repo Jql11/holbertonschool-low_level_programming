8328fe4c4f43210dbc2e255cac9365daaabf7ef5
jql11 <4325@holbertonschool.com>
2022-03-17 19:42:21 -0700

    add space in the text

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,9 +5,11 @@
  *
  * Return: nothing
  */
-void __attribute__((constructor)) before_main()
+void before_main(void) __attribute__((constructor));
+
+void __attribute__((constructor)) before_main(void)
 {
-	printf("You're beat! and yet,you must allow,\n");
+	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
 }
 
