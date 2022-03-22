aebedd333975798ce8a9e527bc7dbee1b1fd1d39
jql11 <4325@holbertonschool.com>
2022-03-21 19:32:36 -0700

    add i <index

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -24,7 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(tmp);
 		return (1);
 	}
-	while (tmp->next != NULL)
+	while (tmp->next != NULL && i < index)
 	{
 		tmp = tmp->next;
 		i++;
