2cff79a0cbc38b4c81dfec64cbd1bee738b1c348
jql11 <4325@holbertonschool.com>
2022-03-21 20:24:56 -0700

    betty

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -29,7 +29,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		tmp = tmp->next;
 		i++;
 	}
-	if (i != index - 1 || tmp->next == NULL) 
+	if (i != index - 1 || tmp->next == NULL)
 		return (-1);
 
 	tmp2 = tmp->next;
