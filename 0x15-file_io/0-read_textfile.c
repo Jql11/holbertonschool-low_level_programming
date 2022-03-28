95d7634b0050482cbf7c164514f2438660246c7c
jql11 <4325@holbertonschool.com>
2022-03-27 20:14:57 -0700

    betty

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,7 +17,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-	
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
@@ -39,8 +38,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
-	
 	close(fd);
 	return (r);
 }
