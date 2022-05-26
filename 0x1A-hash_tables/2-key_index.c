aae3cb699094e368e934b629aa1c1b1e98c1e072
jql11 <4325@holbertonschool.com>
2022-05-25 21:15:24 -0700

    first commit

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -14,7 +14,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size)
 	unsigned long int index;
 
 	if (size == 0)
-		return (0);
+		return (NULL);
 	index = hash_djb2(key);
 	return (index % size);
 
