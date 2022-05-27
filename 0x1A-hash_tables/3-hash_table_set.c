14f8960c0b745113e4e3ffdd87fc41f7a97a5c6b
jql11 <4325@holbertonschool.com>
2022-05-26 17:54:00 -0700

    fix leak

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,36 +12,20 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *item;
-	hash_node_t *current_item;
+	hash_node_t *item, *current_item = NULL;
 	unsigned long int index;
-	char *k, *v;
-
+	
+	if (!ht || key == NULL)
+		return (0);
 	item = malloc(sizeof(hash_node_t));
 	if (item == NULL)
 		return (0);
-	k = strdup(key);
-	if (key == NULL || strlen(key) == 0)
-		return (0);
-	v = strdup(value);
-	if (value == NULL)
-		return (0);
-	item->key = k;
-	item->value = v;
+	item->key = strdup(key);
+	item->value = strdup(value);
 
 	index = key_index((const unsigned char *)key, ht->size);
 	current_item = ht->array[index];
-	if (current_item == NULL) /* this index is not occupued*/
-		ht->array[index] = item;/*insert item to this index*/
-	else
-	{
-		if (strcmp(item->key, key) == 0)
-			strcpy(ht->array[index]->value, value);
-		else
-		{
-			item->next = ht->array[0];
-			ht->array[0] = item;
-		}
-	}
+	item->next = current_item;
+	ht->array[index] = item;/*insert item to this index*/
 	return (1);
 }
