77d7806bad4feb222a620db854ed0d9035bc49c1
jql11 <4325@holbertonschool.com>
2022-05-26 17:59:01 -0700

    betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,7 +14,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *item, *current_item = NULL;
 	unsigned long int index;
-	
+
 	if (!ht || key == NULL)
 		return (0);
 	item = malloc(sizeof(hash_node_t));
