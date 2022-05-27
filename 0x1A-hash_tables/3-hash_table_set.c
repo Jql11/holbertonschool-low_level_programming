e9b02cbe0174d6447856f6c859858516127c2c5c
jql11 <4325@holbertonschool.com>
2022-05-26 19:28:44 -0700

    replace value if key found

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,20 +12,31 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *item, *current_item = NULL;
+	hash_node_t *item = NULL, *current_item = NULL;
 	unsigned long int index;
 
-	if (!ht || key == NULL || strlen(key) == 0)
+	if (!ht || !ht->array || key == NULL || strlen(key) == 0)
 		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	current_item = ht->array[index];
+	while (current_item != NULL)
+	{
+		if (strcmp(current_item->key, key) == 0)
+		{
+			free(current_item->value);
+			current_item->value = strdup(value);
+			return (1);
+		}
+		current_item = current_item->next;
+	}
+
 	item = malloc(sizeof(hash_node_t));
 	if (item == NULL)
 		return (0);
 	item->key = strdup(key);
 	item->value = strdup(value);
-
-	index = key_index((const unsigned char *)key, ht->size);
-	current_item = ht->array[index];
-	item->next = current_item;
+	item->next = ht->array[index];
 	ht->array[index] = item;/*insert item to this index*/
 	return (1);
 }
