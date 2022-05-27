619e9a2f054b9ee9b020b674d1a3b7a9463ec9c7
jql11 <4325@holbertonschool.com>
2022-05-26 18:02:12 -0700

    key cannot be empty string

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,7 +15,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *item, *current_item = NULL;
 	unsigned long int index;
 
-	if (!ht || key == NULL)
+	if (!ht || key == NULL || strlen(key) == 0)
 		return (0);
 	item = malloc(sizeof(hash_node_t));
 	if (item == NULL)
